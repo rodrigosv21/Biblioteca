7428d197bf785edb3cf9ca152e3f52f40ac1738b
rodrigosv21 <rodrigodev21@gmail.com>
2024-11-26 09:14:44 -0300

    criando a rota de cadastro e guardando no banco de dados

--- app.js
+++ app.js
@@ -1,34 +1,48 @@
 const express = require("express");
-
-const { engine } = require ('express-handlebars')
-
-const mysql = require("mysql2")
-
+const { engine } = require("express-handlebars");
+const mysql = require("mysql2");
 const app = express();
+app.use("/bootstrap", express.static("./node_modules/bootstrap/dist"));
+app.use("css", express.static("./css"));
 
-app.use("/bootstrap", express.static("./node_modules/bootstrap/dist"))
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
 
-app.use("css", express.static('./css'))
+app.set("views", "./views");
 
-app.engine('handlebars', engine());
-app.set('view engine', 'handlebars');
-app.set('views', './views');
+app.use(express.json()); //manipulação via json
+app.use(express.urlencoded({ extended: false }));
 
 const conexao = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "login"
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "login",
 });
 
-conexao.connect(function(erro){
-    if (erro) throw erro;
-    console.log("Sucesso!!!")
-})
+conexao.connect(function (erro) {
+  if (erro) throw erro;
+  console.log("Sucesso!!!");
+});
 
+app.get("/", (req, res) => {
+  res.render("formulario");
+});
 
-app.get("/", (req, res) =>{
-    res.render("formulario");
-})
+app.post("/cadastrar", (req, res) => {
+  const nome = req.body.nome;
+  const email = req.body.email;
+  const senha = req.body.senha;
+
+  const sql = `INSERT INTO usuario(nome, email, senha) VALUES ('${nome}', '${email}', ${senha})`;
+  conexao.query(sql, (erro, retorno) => {
+    if (erro) {
+      console.error(erro);
+    } else {
+      console.log("dados inseridos com sucesso");
+    }
+  });
+  res.redirect("/");
+});
 
 app.listen(8080);
