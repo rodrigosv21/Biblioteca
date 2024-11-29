621e359c23f5795600c0d794303d08b368d37242
rodrigosv21 <rodrigodev21@gmail.com>
2024-11-29 11:32:39 -0300

    adicionando rota de cadastrar livros e estlizando a tela de login

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const express = require("express");
-const { engine } = require("express-handlebars");
-const mysql = require("mysql2");
+import express from "express";
+import mysql2 from "mysql2";
+import { engine } from "express-handlebars";
 const app = express();
-app.use("/bootstrap", express.static("./node_modules/bootstrap/dist"));
-app.use("css", express.static("./css"));
 
+app.use("/bootstrap", express.static("./node_modules/bootstrap/dist"));
+app.use("/css", express.static("./css"));
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
-
 app.set("views", "./views");
-
-app.use(express.json()); //manipulação via json
+app.use(express.json()); //manipulação via jsonJ
 app.use(express.urlencoded({ extended: false }));
 
-const conexao = mysql.createConnection({
+//conexão com o banco de dados mysql
+const conexao = mysql2.createConnection({
   host: "localhost",
   user: "root",
   password: "",
@@ -22,17 +21,37 @@ const conexao = mysql.createConnection({
 
 conexao.connect(function (erro) {
   if (erro) throw erro;
-  console.log("Sucesso!!!");
+  console.log("Banco de dados funcionando");
 });
 
+//rotas de renderização da rota
 app.get("/", (req, res) => {
-  res.render("formulario");
+  res.render("inicio");
 });
 
-app.post("/cadastrar", (req, res) => {
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/entrar", (req, res) => {
+  res.render("entrar");
+});
+
+app.get("/cadastrarlivros", (req, res) => {
+  res.render("livros");
+});
+
+app.post("/entrar", (req, res) => {});
+
+//rotas de cadastrar
+app.post("/login", (req, res) => {
   const nome = req.body.nome;
   const email = req.body.email;
   const senha = req.body.senha;
+
+  if (!nome || !email || !senha) {
+    return res.status(400).send("preencher todos os dados.");
+  }
 
   const sql = `INSERT INTO usuario(nome, email, senha) VALUES ('${nome}', '${email}', ${senha})`;
   conexao.query(sql, (erro, retorno) => {
@@ -40,6 +59,28 @@ app.post("/cadastrar", (req, res) => {
       console.error(erro);
     } else {
       console.log("dados inseridos com sucesso");
+    }
+  });
+  res.redirect("/entrar");
+});
+
+//cadastrar livros
+app.post("/cadastrarlivros", (req, res) => {
+  const titulo = req.body.titulo;
+  const autor = req.body.autor;
+  const generos = req.body.generos;
+  const npaginas = req.body.npaginas;
+
+  if (!titulo || !autor || !generos || !npaginas) {
+    return res.status(400).send("preencher todos os dados.");
+  }
+
+  const sql = `INSERT INTO livros(titulo, autor, generos, npaginas) VALUES ('${titulo}', '${autor}', '${generos}', ${npaginas})`;
+  conexao.query(sql, (erro, retorno) => {
+    if (erro) {
+      console.error(erro);
+    } else {
+      console.log("livro cadastrado");
     }
   });
   res.redirect("/");
